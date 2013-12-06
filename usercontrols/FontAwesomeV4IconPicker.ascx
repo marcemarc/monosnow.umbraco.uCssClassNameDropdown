8477e44ef63446b1982b9d4272f5e7f7af953cd1
Marc Goodson <marc@snowmoon.co.uk>
2013-12-06 14:35:38 +0000

    Support for Font-awesome v4

---
+++ usercontrols/FontAwesomeV4IconPicker.ascx
@@ -0,0 +1,26 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="FontAwesomeV4IconPicker.ascx.cs" Inherits="monosnow.umbraco.uCssClassNameDropdown.usercontrols.FontAwesomeV4IconPicker" %>
+<asp:ListView ID="lvIcons" runat="server" ItemPlaceholderID="itemPlaceHolder" GroupItemCount="4" GroupPlaceholderID="groupPlaceHolder">
+<LayoutTemplate>
+<div style="height:400px; width:650px;overflow:scroll">
+<asp:PlaceHolder ID="groupPlaceHolder" runat="server"></asp:PlaceHolder>
+</div>
+</LayoutTemplate>
+<GroupTemplate>
+<div style="clear:both"></div>
+<div>
+<asp:PlaceHolder ID="itemPlaceHolder" runat="server"></asp:PlaceHolder>
+</div>
+</GroupTemplate>
+<ItemTemplate>
+<a style="text-decoration:none; font-size:1.4em;color:#000000" href="#" onclick="setDropdownValue('<%#Container.DataItem %>');"><div style="width:150px;height:50px;float:left;"><i class="icon-<%#Container.DataItem %>"></i> <%#Container.DataItem %></div></a>
+</ItemTemplate>
+</asp:ListView>
+<asp:DropDownList ID="ddlIcons" runat="server"></asp:DropDownList>
+
+<script type="text/javascript">
+    function setDropdownValue(selectedValue) {
+        document.getElementById("<%=ddlIcons.ClientID %>").value = selectedValue;       
+    }
+
+
+</script>
---
+++ usercontrols/FontAwesomeV4IconPicker.ascx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using monosnow.umbraco.uCssClassNameDropdown.Services;
+using System.Web.UI.HtmlControls;
+using umbraco.editorControls.userControlGrapper;
+
+namespace monosnow.umbraco.uCssClassNameDropdown.usercontrols
+{
+    public partial class FontAwesomeV4IconPicker : System.Web.UI.UserControl, IUsercontrolDataEditor 
+    {
+        protected override void OnPreRender(EventArgs e) 
+   {
+        bool linkIncluded = false;
+        foreach (Control c in Page.Header.Controls)
+        {
+            if (c.ID == "FontAwesomeStylesheet")
+            {
+                linkIncluded = true;
+            }
+        }
+        if (!linkIncluded)
+        {
+            HtmlGenericControl csslink = new HtmlGenericControl("link");
+            csslink.ID = "FontAwesomeStylesheet";
+            csslink.Attributes.Add("href", "/css/font-awesome.css");
+            csslink.Attributes.Add("type", "text/css");
+            csslink.Attributes.Add("rel", "stylesheet");
+            Page.Header.Controls.Add(csslink);
+        }
+    }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!Page.IsPostBack)
+            {
+                ClassNameRetrievalService _service = new ClassNameRetrievalService("/css/font-awesome.css", "large", @"\.fa-([^:]*?):before");
+                lvIcons.DataSource = _service.GetClassNames();
+                lvIcons.DataBind();
+                ddlIcons.DataSource = _service.GetClassNames();
+                ddlIcons.DataBind();
+                ddlIcons.Items.Insert(0, new ListItem("Choose...", ""));
+
+                
+            }
+
+        }
+
+        #region IUsercontrolDataEditor Members
+
+        public object value
+        {
+            get
+            {
+                return ddlIcons.SelectedValue;
+            }
+            set
+            {
+                if (!String.IsNullOrEmpty(value.ToString()))
+                {
+                    ddlIcons.SelectedValue = value.ToString();
+                }
+            }
+        }
+
+        #endregion
+    }
+}
